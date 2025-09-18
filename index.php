11e0a06b259cb46435076efa48d331b64e54b24a
villeyoo <nabilmaulanaaalhakim@gmail.com>
2025-09-18 23:30:07 +0700

    fixedfix

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
 
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
 require __DIR__.'/vendor/autoload.php';
 
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-$app->bind('path.public', function() {
-    return __DIR__.'/public';
-});
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/bootstrap/app.php')
+    ->handleRequest(Request::capture());
