401f336585210790232bc39c714dcf78044c78db
villeyoo <nabilmaulanaaalhakim@gmail.com>
2025-09-18 23:08:00 +0700

    tesss

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-require __DIR__ . '/public/index.php';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->bind('path.public', function() {
+    return __DIR__.'/public';
+});
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
